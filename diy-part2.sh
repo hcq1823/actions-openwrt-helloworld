d34e1bb8844e49291662ea080132298e78523c96
hcq1823 <2501802233@qq.com>
2026-01-17 00:42:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,5 +8,9 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+# 修改openwrt登陆地址
+sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
+
+#设置登录密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
